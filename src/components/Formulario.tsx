2c3ba0becfbb1a5fa15bbcdd85c5f6c3091b8f2b
Efrain <esavila@uniempresarial.edu.co>
2024-05-20 19:58:11 -0500

    chore: Update login form structure and styles

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 
 const Formulario: React.FC = () => {
   return (
-    <form>
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="card" >
+        <div className="card-body">
+          <h5 className="card-title">Iniciar Sesión</h5>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Password</label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+            <button type="submit" className="btn btn-primary w-100">Submit</button>
+          </form>
+        </div>
       </div>
-      <div className="mb-3">
-        <label htmlFor="password" className="form-label">Password</label>
-        <input type="password" className="form-control" id="password" />
-      </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+    </div>
   );
 };
 
