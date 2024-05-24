beb9626ef6c724b99a30a538a6a7b1aa79a2c4ed
Efrain <esavila@uniempresarial.edu.co>
2024-05-23 19:32:30 -0500

    feat: Update AWS configuration with new User Pool ID and Client ID

--- src/services/awsConfig.ts
+++ src/services/awsConfig.ts
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
-  UserPoolId: 'us-east-1_example', // Reemplaza con tu User Pool ID
-  ClientId: 'exampleclientid12345' // Reemplaza con tu App Client ID
+  UserPoolId: 'us-east-1_LpuMyywK9', // Reemplaza con tu User Pool ID
+  ClientId: '84b864a8-40f1-7028-ad7c-e9572cdb0069' // Reemplaza con tu App Client ID
 };
 
 export const userPool = new CognitoUserPool(poolData);
